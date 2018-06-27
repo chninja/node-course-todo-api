d7b466766b927f5d5ed916e476c10a4227657bc3
chninja <catherine@192.168.0.190>
2018-06-27 09:30:16 -0300

    add patch todos

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
-
+const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo}  = require('./models/todo');
@@ -76,7 +76,7 @@ app.delete('/todos/:id', (req, res) => {
 
 
 	//validate the id -> not valid return 404
-	if (ObjectID.isValid(id)) {
+	if (!ObjectID.isValid(id)) {
 		return res.status(404).send();
 	}
 
@@ -85,7 +85,7 @@ app.delete('/todos/:id', (req, res) => {
 			return res.status(404).send();
 		}
 
-		res.send(todo);
+		res.send({todo});
 
 	}).catch((e) => {
 		res.status(400).send();
@@ -93,6 +93,34 @@ app.delete('/todos/:id', (req, res) => {
 
 });
 
+
+
+app.patch('/todos/:id', (req,res)=> {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text','completed']);
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	if(_.isBoolean(body.completed) && body.completed){
+		body.completedAt = new Date().getTime();
+	}else{
+		body.completed = false;
+		body.completeAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body},{new: true}).then((todo) =>{
+		if(!todo){
+			return res.status(400).send();
+		}
+
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	})
+
+});
 
 
 app.listen(port, () => {
