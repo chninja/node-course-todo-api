fbd21d03dde346a0d40ee7cd692863848ad76eda
chninja <catherine@192.168.0.190>
2018-06-28 12:48:21 -0300

    hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,36 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 
-var data = {
-	id:10
-};
 
-var token = jwt.sign(data, '123abc');
-var decode = jwt.verify(token, '123abc');
+
+var password = '123abc';
+
+
+bcrypt.genSalt(10, (err,salt) => {
+	bcrypt.hash(password, salt, (err, hash) => {
+		console.log(hash);
+	});
+});
+
+var hashedPassword = '$2a$10$frZxJoK0gSvzEeFk7WTF2u0qvMScFuZbB/JkXEFjwuoIV2W6zT7vy';
+
+bcrypt.compare(password, hashedPassword,(err,res) => {
+	console.log(res);
+});
+
+
+
+
+
+
+// var data = {
+// 	id:10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// var decode = jwt.verify(token, '123abc');
 
 
 // var message = 'I am user number 3';
